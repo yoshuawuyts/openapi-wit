7cd3e38ebfec9fa8d98aa0ba8381a3288c32af98
Jesse Stuart <email@jessestuart.ca>
2024-08-26 07:38:32 -0400

    chore: fix doc comment

--- crates/wasi-async-runtime/src/lib.rs
+++ crates/wasi-async-runtime/src/lib.rs
@@ -1,6 +1,6 @@
 //! A single-threaded native runtime for WASI 0.2
 //!
-//! The way to use this is to call [`block_on`] to obtain an instance of
+//! The way to use this is to call [`block_on()`] to obtain an instance of
 //! [`Reactor`]. You can then share the reactor in code that needs it to insert
 //! instances of
 //! [`wasi::Pollable`](https://docs.rs/wasi/latest/wasi/io/poll/struct.Pollable.html).
